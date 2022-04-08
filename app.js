012b6f97b1cf23b47e9b2cb64a055a1088506d1e
Punit Tewani <tewanipunit1233@gmail.com>
2022-04-08 14:48:00 +0530

    Initial Commits

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+//Middlewares
+
+//Server Creation
+const app = express();
+
+
+//Routing
+
+
+
+
+// Server Listening
+app.listen(process.env.PORT,()=>{
+    console.log("TrackBit Server Running on Port 5000");
+})
