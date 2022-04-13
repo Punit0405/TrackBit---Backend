0492cb961a13fa7986b3aa3409b7e68df9030dcb
Punit Tewani <tewanipunit1233@gmail.com>
2022-04-13 16:09:10 +0530

    Some Changes applie in app.js

--- DBConnection/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect(process.env.DATABASE_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(console.log("Connected To Trackbit Database")).catch((err)=>{console.log(err)});
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors=require('cors')
 require('./DBconnection/connection');
 const userRoutes=require('./Routes/userRouting');
 const habitRoutes =require('./Routes/habitRouting')
+const todoRoutes = require('./Routes/todoRouting');
 
 
 //Server Creation
@@ -23,6 +24,7 @@ app.use(cors({
 //Routing
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/habit',habitRoutes)
+app.use('/api/v1/todo',todoRoutes)
 
 // Server Listening
 app.listen(process.env.PORT,()=>{
