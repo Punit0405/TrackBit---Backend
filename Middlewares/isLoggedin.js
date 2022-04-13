8226fe0bf4b6964fba6f3b9937c1773b2e3c327d
Punit Tewani <tewanipunit1233@gmail.com>
2022-04-13 15:20:47 +0530

    Middleware Loggedin Changed

--- Middlewares/isLoggedin.js
+++ Middlewares/isLoggedin.js
@@ -6,8 +6,18 @@ const isLoggedin = async (req,res,next)=>{
         return res.status(400).json({status:false,data:"You are Not Loggedin"})
     }
     const authToken=req.headers.authtoken;
-    const loggedUser = await jwt.verify(authToken,process.env.JWT_USER_LOGIN_SECRET_KEY);
-    req.user=loggedUser;
-    next();
+    try {
+        const loggedUser = await jwt.verify(authToken,process.env.JWT_USER_LOGIN_SECRET_KEY);
+         if(!loggedUser.id){
+             return res.status(401).json({status:false,data:"Not a valid User"})
+         }
+        req.user=loggedUser;
+        next();
+    } catch (error) {
+        
+        return res.status(400).json({status:false,data:"Invalid Token"})
+        
+    }
+    
 }
 module.exports=isLoggedin;
