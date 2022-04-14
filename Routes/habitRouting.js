021abfeccf9811cbb36bc11309d72e23353083a0
Punit Tewani <tewanipunit1233@gmail.com>
2022-04-14 15:54:09 +0530

    Validations Implemented

--- Routes/habitRouting.js
+++ Routes/habitRouting.js
@@ -2,15 +2,16 @@ const express=require('express');
 const router= express.Router();
 const isLoggedin=require('../Middlewares/isLoggedin');
 const HabitController = require("../Controllers/HabitController");
+const validator= require("../Validations/validator")
 
 //Route for Adding Habbit
-router.route("/addhabit").post(isLoggedin,HabitController.addHabit);
+router.route("/addhabit").post(isLoggedin,validator.validateHabit,HabitController.addHabit);
 
 //Route for Fetching Habit
 router.route("/fetchhabit").get(isLoggedin,HabitController.fetchHabits)
 
 //Route for Updating Habit
-router.route("/updateHabit/:id").put(isLoggedin,HabitController.updateHabit)
+router.route("/updateHabit/:id").put(isLoggedin,validator.validateHabit,HabitController.updateHabit)
 
 //Route for Deleting Habit
 router.route("/deleteHabit/:id").delete(isLoggedin,HabitController.deleteHabit)
