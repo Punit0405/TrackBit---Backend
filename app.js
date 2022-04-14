063759e753f4ccab18dd36432f2efe1ec3459b6f
Punit Tewani <tewanipunit1233@gmail.com>
2022-04-14 14:46:56 +0530

    Revert "Merge branch 'release-branch' into login-dailies-punit"
    This reverts commit ab60663f69e1aa35afba620088e4f6ff7148ebc6, reversing
    changes made to d856629106f9f67bf7592715251bc8767510670e.

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const cors=require('cors')
 require('./DBconnection/connection');
 const userRoutes=require('./Routes/userRouting');
 const dailiesRoutes= require('./Routes/dailiesRouting');
-const habitRoutes =require('./Routes/habitRouting')
-const todoRoutes = require('./Routes/todoRouting');
-
 
 
 //Server Creation
@@ -24,12 +21,8 @@ app.use(cors({
   }))
 
 //Routing
-
 app.use('/api/v1/user',userRoutes);
-app.use('/api/v1/daily',dailiesRoutes);
-app.use('/api/v1/habit',habitRoutes);
-app.use('/api/v1/todo',todoRoutes);
-
+app.use('/api/v1/daily',dailiesRoutes)
 
 // Server Listening
 app.listen(process.env.PORT,()=>{
